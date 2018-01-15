d2195fb21d7becd1743fb165327f8315f759ff2d
Shannon Archer <shannon.archer@zoro.com>
2018-01-15 14:40:40 -0600

    disabled everything except for the references section

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports = {
   extends: [
-    './rules/best-practices',
-    './rules/errors',
-    './rules/node',
-    './rules/style',
-    './rules/variables',
+    // './rules/best-practices',
+    // './rules/errors',
+    // './rules/node',
+    // './rules/style',
+    // './rules/variables',
     './rules/es6',
-    './rules/imports',
+    // './rules/imports',
   ].map(require.resolve),
   parserOptions: {
     ecmaVersion: 2017,
